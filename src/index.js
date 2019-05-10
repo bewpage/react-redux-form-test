0c9d59bc8343931988c59d6c562eaa9a2757246a
bewpage <b.jedlinski@bewpage.com>
2019-05-10 10:37:40 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const middleware = applyMiddleware(thunk);
 
 const store = createStore(
   reducer,
-  compose(middleware, window.devToolsExtension ? window.devToolsExtension() : f => f)
+  compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
 );
 
 ReactDOM.render(
